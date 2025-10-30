e7b593ac1f5e267d20d056d6fab96cf8fdc4fa0d
Galuxium <cofounder.galuxium@gmail.com>
2025-10-31 00:43:57 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * Minimal Next.js 15 configuration with empty image domains
+ * Required for App Router projects using default image handling
+ */
+module.exports = {
+  images: {
+    domains: []
+  }
+};
